804d572ce6b2b6e2ec72b5c6ed9e2440f72e5967
Jaseyacey <41111290+Jaseyacey@users.noreply.github.com>
2024-12-16 11:46:30 +0000

    22-check-user-session

--- src/Config/supabase.ts
+++ src/Config/supabase.ts
@@ -1,8 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  });
 
 export default supabase
--- src/Screens/SplashScreen.tsx
+++ src/Screens/SplashScreen.tsx
@@ -1,29 +1,49 @@
-import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import React, { useEffect } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import supabase from '@config/supabase';
+import { SplashScreenProps } from 'src/Types/types';
 
-const SplashScreen = ({navigation}) => {
-    setTimeout(() => {
-        navigation.navigate('SignUpScreen');
-    }, 2500);
+const SplashScreen: React.FC<SplashScreenProps> = ({ navigation }) => {
+    
+    const checkUser = async () => {
+        console.log('SplashScreen');
+        try {
+          const { data: { session }, error } = await supabase.auth.getSession();
+          if (error) {
+            throw error;
+          }
+          if (session?.user) {
+            await AsyncStorage.setItem('userUuid', session.user.id);
+            navigation.replace('HomeScreen');
+          } else {
+            navigation.replace('SignInScreen');
+          }
+        } catch (error) {
+          console.error('Error checking user session:', error);
+          navigation.replace('signUpScreen');
+        }
+      };
+      
+
+    useEffect(() => {
+            checkUser();
+    }, []);
+
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.header}>Splash Screen</Text>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color="#0000ff" />
+            <Text>Loading...</Text>
+        </View>
     );
 };
 
-const styles = StyleSheet.create({
+const styles = {
     container: {
         flex: 1,
-        padding: 16,
-        backgroundColor: '#fff',
+        justifyContent: 'center' as 'center',
+        alignItems: 'center' as 'center',
     },
-    header: {
-        fontSize: 24,
-        fontWeight: '500',
-        color: '#333',
-        marginBottom: 16,
-    },
-});
+};
 
 export default SplashScreen;
