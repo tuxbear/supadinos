ac867d2f6814e74aa4fced3147c06bcf15e0ed77
Jaseyacey <41111290+Jaseyacey@users.noreply.github.com>
2024-12-16 10:46:34 +0000

    20-add-verification-screen

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import supabase from '@config/supabase';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
+    const [userUuid, setUserUuid] = useState('');
+    useEffect(() => {
+        supabase.auth.getUser().then((user) => setUserUuid(user.data.user.id));
+        supabase.from('user_profile').select().then(({ data, error }) => {
+            console.log('data', data);
+            console.log('error', error);
+        });
+    }, []);
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Home Screen</Text>
+            <Text>User UUID: {userUuid}</Text>
         </View>
     );
 };
